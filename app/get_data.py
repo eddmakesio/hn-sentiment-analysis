0427fb0806b2c479c6329032c4d510f0d470c4ae
eddmakesio <eddmakes@icloud.com>
2023-09-24 17:10:57 +0200

    Save stories and comments

--- app/get_data.py
+++ app/get_data.py
@@ -1,43 +1,68 @@
 import requests
 import sqlite3
 
-
 con = sqlite3.connect("./db/hn_sentiment_analysis.db")
 
 API = "https://hacker-news.firebaseio.com/v0"
 
 def get_best_stories():
-    r = requests.get(f"{API}/topstories.json")
-    return r.json()
+    try: 
+        r = requests.get(f"{API}/topstories.json")
+        return r.json()
+    except:
+        print(f"Unable to get top stories")
 
 def get_story_data(story_id: int):
-    r = requests.get(f"{API}/item/{story_id}.json")
-    return r.json()
+    try: 
+        r = requests.get(f"{API}/item/{story_id}.json")
+        return r.json()
+    except:
+        print(f"Unable to get story #{story_id}")
 
 def get_stories_comments(story_id: int):
-    r = requests.get(f"{API}/item/{story_id}.json")
-    comment_ids = r.json()["kids"]
+    try: 
+        r = requests.get(f"{API}/item/{story_id}.json")
+        comment_ids = r.json()["kids"]
 
-    comments = []
+        comments = []
 
-    for comment_id in comment_ids:
-        r = requests.get(f"{API}/item/{comment_id}.json")
-        comments.append(r.json())
+        for comment_id in comment_ids:
+            r = requests.get(f"{API}/item/{comment_id}.json")
+            comments.append(r.json())
 
-    return comments
+        return comments
+    except:
+        print(f"Unable to get comments for story #{story_id}")
 
 def save_story(story):
-    cur = con.cursor()
-    sql_query = "INSERT OR IGNORE INTO story (hn_story_id, title, created) VALUES(?, ?, ?)"
-    insert_data = (story["id"], story["title"], story["time"])
-    cur.execute(sql_query, insert_data)
-    con.commit()
-    cur.close()
+    try: 
+        cur = con.cursor()
+        sql_query = "INSERT OR IGNORE INTO story (hn_story_id, title, created) VALUES(?, ?, ?)"
+        insert_data = (story["id"], story["title"], story["time"])
+        cur.execute(sql_query, insert_data)
+        con.commit()
+        cur.close()
+    except: 
+        print(f"Unable to save story #{story['id']}: {story}")
+
+def save_comment(comment):
+    try: 
+        cur = con.cursor()
+        sql_query = "INSERT OR IGNORE INTO comment (hn_comment_id, hn_story_id, contents, created) VALUES(?, ?, ?, ?)"
+        insert_data = (comment["id"], comment["parent"], comment["text"], comment["time"])
+        cur.execute(sql_query, insert_data)
+        con.commit()
+        cur.close()
+    except:
+        print(f"Unable to save comment #{comment['id']}: {comment}")
 
 if __name__ == '__main__':    
     best_stories = get_best_stories()
     for story_id in best_stories:
-        print(f'Saving {story_id}...')
+        print(f'Saving story #{story_id}...')
         story = get_story_data(story_id)
         save_story(story)
-        
+        comments = get_stories_comments(story["id"])
+        for comment in comments:
+            print(f'Saving comment #{comment["id"]}...')
+            save_comment(comment)
--- db/sql/tables.sql
+++ db/sql/tables.sql
@@ -7,15 +7,9 @@ CREATE TABLE IF NOT EXISTS story (
 
 CREATE TABLE IF NOT EXISTS comment (
     hn_comment_id       INTEGER     PRIMARY KEY,
+    hn_story_id         INTEGER     NOT NULL        REFERENCES story(hn_story_id),
     contents            TEXT        NOT NULL,
     created             INTEGER     NOT NULL,
     sentiment           FLOAT,
     UNIQUE(hn_comment_id)
-);
-
-CREATE TABLE IF NOT EXISTS comment_story (
-    comment_story_id    INTEGER     PRIMARY KEY,
-    hn_story_id         INTEGER     NOT NULL        REFERENCES story(hn_story_id),
-    hn_comment_id       INTEGER     NOT NULL        REFERENCES comment(hn_comment_id),
-    UNIQUE(hn_story_id, hn_comment_id)
 );
